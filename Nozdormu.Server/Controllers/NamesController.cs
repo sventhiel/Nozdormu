dc405b554ed3f788d7547c156d2bf052e0010eea
Sven Thiel <m6thsv2@googlemail.com>
2022-11-18 00:40:13 +0100

    update general structure

---
+++ Nozdormu.Server/Controllers/NamesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NameParser;
+
+namespace Nozdormu.Server.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class NamesController : ControllerBase
+    {
+        [HttpGet("Name")]
+        public HumanName GetName(string name)
+        {
+            return new HumanName(name);
+        }
+    }
+}
