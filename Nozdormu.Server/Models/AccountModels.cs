dc9cdda3ac91d8acf05ce88601b12c508aab696b
Sven Thiel <m6thsv2@googlemail.com>
2022-12-20 15:06:41 +0100

    rename properties

--- Nozdormu.Server/Models/AccountModels.cs
+++ Nozdormu.Server/Models/AccountModels.cs
@@ -1,9 +1,50 @@
-﻿namespace Nozdormu.Server.Models
+﻿using Nozdormu.Server.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Nozdormu.Server.Models
 {
     public class CreateAccountModel
     {
+        [Required]
+        [Display(Name = "Host")]
+        public string Host { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required, Compare("Password")]
+        [Display(Name = "ConfirmPassword")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Prefix")]
+        public string Prefix { get; set; }
+    }
+
+    public class ReadAccountModel
+    {
+        public long Id { get; set; }
         public string Password { get; set; }
         public string Prefix { get; set; }
-        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Host { get; set; }
+
+        public static ReadAccountModel Convert(Account account)
+        {
+            return new ReadAccountModel()
+            {
+                Id = account.Id,
+                Name = account.Name,
+                Password = account.Password,
+                Prefix = account.Prefix,
+                Host = account.Host
+            };
+        }
     }
 }
--- Nozdormu.Server/Models/UserModels.cs
+++ Nozdormu.Server/Models/UserModels.cs
@@ -1,8 +1,54 @@
-﻿namespace Nozdormu.Server.Models
+﻿using Nozdormu.Server.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Nozdormu.Server.Models
 {
     public class LoginUserModel
     {
-        public string Username { get; set; }
+        public string Name { get; set; }
         public string Password { get; set; }
+    }
+
+    public class ReadUserModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Pattern { get; set; }
+        public long? AccountId { get; set; }
+
+        public static ReadUserModel Convert(User user)
+        {
+            return new ReadUserModel()
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Pattern = user.Pattern,
+                AccountId = user.Account?.Id
+            };
+        }
+    }
+
+    public class CreateUserModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required, Compare("Password")]
+        [Display(Name = "ConfirmPassword")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Pattern")]
+        public string Pattern { get; set; }
+
+        [Required]
+        [Display(Name = "AccountId")]
+        public long? AccountId { get; set; }
     }
 }
